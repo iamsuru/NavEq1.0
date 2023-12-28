0864924538bd4492e1b622f11a89b50b48ea0c66
iamsuru <suryanshshrivastava6364@gmail.com>
2023-12-28 18:26:35 +0530

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Background from './components/Background';
 import GroundFloorMap from './components/maps/GroundFloorMap';
 function App() {
   return (
